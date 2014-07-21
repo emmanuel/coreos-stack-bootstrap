c0e5f5850a25cb871dd04903a128abd0994d58f4
Emmanuel Gomez <emmanuel@gomez.io>
2014-07-21 13:52:20 -0700

    Parameterize EC2 key pair in create-stack.sh.

--- create-stack.sh
+++ create-stack.sh
@@ -4,18 +4,20 @@ STACK_NAME=${1:-"CoreOS-test-$RANDOM"}
 STACK_DISCOVERY_URL=${2:-`curl -s https://discovery.etcd.io/new`}
 STACK_INSTANCE_TYPE=${3:-m3.medium}
 STACK_CLUSTER_SIZE=${4:-3}
+STACK_KEY_PAIR=${5:-coreos-beta}
 
 echo "Creating CloudFormation Stack with these parameters:"
 echo "Name: $STACK_NAME"
 echo "Discovery URL: $STACK_DISCOVERY_URL"
 echo "Instance Type x Cluster Size: $STACK_INSTANCE_TYPE x $STACK_CLUSTER_SIZE"
+echo "EC2 Key Pair: $STACK_KEY_PAIR"
 
 aws cloudformation create-stack \
---stack-name $STACK_NAME \
---template-body file://$PWD/cloudformation-template.json \
---parameters "ParameterKey=InstanceType,ParameterValue=$STACK_INSTANCE_TYPE,UsePreviousValue=false" \
-"ParameterKey=ClusterSize,ParameterValue=$STACK_CLUSTER_SIZE,UsePreviousValue=false" \
-"ParameterKey=DiscoveryURL,ParameterValue=$STACK_DISCOVERY_URL,UsePreviousValue=false" \
-"ParameterKey=AdvertisedIPAddress,ParameterValue=private,UsePreviousValue=false" \
-"ParameterKey=AllowSSHFrom,ParameterValue=0.0.0.0/0,UsePreviousValue=false" \
-"ParameterKey=KeyPair,ParameterValue=coreos-beta,UsePreviousValue=false"
+  --stack-name $STACK_NAME \
+  --template-body file://$PWD/cloudformation-template.json \
+  --parameters "ParameterKey=InstanceType,ParameterValue=$STACK_INSTANCE_TYPE,UsePreviousValue=false" \
+  "ParameterKey=ClusterSize,ParameterValue=$STACK_CLUSTER_SIZE,UsePreviousValue=false" \
+  "ParameterKey=DiscoveryURL,ParameterValue=$STACK_DISCOVERY_URL,UsePreviousValue=false" \
+  "ParameterKey=AdvertisedIPAddress,ParameterValue=private,UsePreviousValue=false" \
+  "ParameterKey=AllowSSHFrom,ParameterValue=0.0.0.0/0,UsePreviousValue=false" \
+  "ParameterKey=KeyPair,ParameterValue=$STACK_KEY_PAIR,UsePreviousValue=false"
