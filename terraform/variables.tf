de5bd54ffce4f28677e669f7601dc4e9111f04e8
Emmanuel Gomez <emmanuel@gomez.io>
2014-09-04 16:12:56 -0700

    Remove default value for etcd_discovery_url.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
-variable "etcd_discovery_url" {}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "etcd_discovery_url" {}
 
 variable "aws_instance_type" {
   default = "m3.medium"
