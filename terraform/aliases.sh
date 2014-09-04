6b7b36473963f4b519fa30b0f43e52895445e21c
Paul Payne <paul@payne.io>
2014-09-04 16:42:02 -0700

    A better way to create the terraform etcd_discovery_url. And some aliases to source.

--- terraform/aliases.sh
+++ terraform/aliases.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ ! -f "etcd_discovery_url" ]; then
+  echo "etcd_discovery_url = \"$(curl -s https://discovery.etcd.io/new)\"" >> etcd_discovery_url.tfvars
+fi
+
 alias tfplan='terraform plan --var-file=terraform.tfvars --var-file etcd_discovery_url.tfvars'
 alias tfapply='terraform apply --var-file=terraform.tfvars --var-file etcd_discovery_url.tfvars'
 alias tfdestroyplan='terraform plan --var-file=terraform.tfvars --var-file etcd_discovery_url.tfvars -destroy --out=destroy.tfplan'
