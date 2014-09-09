38a69633e6fa2e81453667a5d3990cfca124c69b
Paul Payne <paul@payne.io>
2014-09-09 14:32:38 -0700

    Add fleet influxdb db_create to aws/launch_units.

--- aws/launch_units.sh
+++ aws/launch_units.sh
@@ -12,6 +12,7 @@ fi
 
 fleetctl start influxdb/influxdb@1.service
 fleetctl start influxdb/influxdb.presence@1.service
+fleetctl start influxdb/influxdb.db_create.service
 
 fleetctl start cadvisor/cadvisor.service
 fleetctl start sysinfo_influxdb/sysinfo_influxdb.service
