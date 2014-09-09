8812b0787e8fbe9c07a288b442fb6ade13fb71e5
Emmanuel Gomez <emmanuel@gomez.io>
2014-09-09 15:00:18 -0700

    Get InfluxDB database creation working.

--- aws/launch_units.sh
+++ aws/launch_units.sh
@@ -12,7 +12,7 @@ fi
 
 fleetctl start influxdb/influxdb@1.service
 fleetctl start influxdb/influxdb.presence@1.service
-fleetctl start influxdb/influxdb.db_create.service
+fleetctl start influxdb/influxdb.db_create@1.service
 fleetctl start influxdb/influxdb.elb@1.service
 
 fleetctl start cadvisor/cadvisor.service
