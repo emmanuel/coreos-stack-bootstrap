0013c0228c693031f1ee10fd2a660f2e4d883587
Emmanuel Gomez <emmanuel@gomez.io>
2014-09-15 23:05:12 -0700

    Update AMIs to CoreOS v438.0.0.
    This releases includes an update cloud-init that
    should support YAML merge. This means that etc
    discovery URLs and fleet metadata can be dynamically
    injected into the CoreOS cloud-config YAML via
    Terraform variables and outputs. Yay!

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,16 +17,17 @@ variable "allow_ssh_from" {
   default = "0.0.0.0/0"
 }
 
+# get updates at https://s3.amazonaws.com/coreos.com/dist/aws/coreos-alpha-hvm.template
 variable "amis" {
   default = {
-    ap-northeast-1 = "ami-59371558"
-    sa-east-1 = "ami-f51ab1e8"
-    ap-southeast-2 = "ami-1b98f821"
-    ap-southeast-1 = "ami-0cab8e5e"
-    us-east-1 = "ami-c4fe5cac"
-    us-west-2 = "ami-25276615"
-    us-west-1 = "ami-d7999792"
-    eu-west-1 = "ami-9cff5aeb"
+    ap-northeast-1 = "ami-19210d18"
+    sa-east-1 = "ami-e9ba10f4"
+    ap-southeast-2 = "ami-1706652d"
+    ap-southeast-1 = "ami-26250174"
+    us-east-1 = "ami-0a6ac562"
+    us-west-2 = "ami-49febf79"
+    us-west-1 = "ami-09c8c14c"
+    eu-west-1 = "ami-5070d427"
   }
 }
 variable "aws_ec2_keypair" {
