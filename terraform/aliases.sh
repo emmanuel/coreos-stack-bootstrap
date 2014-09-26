75e500ff349e6aa0ed6511bd914c302385774b86
Emmanuel Gomez <emmanuel@gomez.io>
2014-09-26 16:06:31 -0700

    Combine environment and discovery url tfvars.

--- terraform/aliases.sh
+++ terraform/aliases.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-alias tfplan='terraform plan --var-file=terraform.tfvars --var-file etcd_discovery_url.tfvars --var-file environment.tfvars'
-alias tfapply='terraform apply --var-file=terraform.tfvars --var-file etcd_discovery_url.tfvars --var-file environment.tfvars'
-alias tfdestroyplan='terraform plan --var-file=terraform.tfvars --var-file etcd_discovery_url.tfvars --var-file environment.tfvars -destroy --out=destroy.tfplan'
+alias tfplan='terraform plan --var-file=terraform.tfvars --var-file cluster_values.tfvars'
+alias tfapply='terraform apply --var-file=terraform.tfvars --var-file cluster_values.tfvars'
+alias tfdestroyplan='terraform plan --var-file=terraform.tfvars --var-file cluster_values.tfvars -destroy --out=destroy.tfplan'
