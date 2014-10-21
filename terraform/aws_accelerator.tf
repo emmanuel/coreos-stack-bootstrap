62d05492ef8d521801f107bde6965d7885f076a0
Emmanuel Gomez <emmanuel@gomez.io>
2014-10-21 16:55:09 -0700

    Add an ELB and security groups for exposing APIs.

--- terraform/aws_accelerator.tf
+++ terraform/aws_accelerator.tf
@@ -164,6 +164,49 @@ resource "aws_security_group" "elb-ingress" {
   }
 }
 
+resource "aws_security_group" "elb-api" {
+  name = "Sandbox API ELB (${var.environment})"
+  description = "Sandbox API ELB (${var.environment})"
+
+  # nsandbox-fake-api-store
+  ingress {
+    from_port = 80
+    to_port =  80
+    protocol = "tcp"
+    cidr_blocks = [ "${var.allow_ssh_from}" ]
+  }
+}
+
+resource "aws_elb" "api" {
+  name = "api-${var.environment}"
+  availability_zones = [ "us-west-2a", "us-west-2b", "us-west-2c" ]
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = 19110
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 10
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:19110/stores/1"
+    interval = 30
+  }
+
+  security_groups = [ "${aws_security_group.elb-api.id}" ]
+}
+
+resource "aws_route53_record" "api" {
+  zone_id = "${var.aws_route53_zone_id_cloud_nlab_io}"
+  name = "api.${var.environment}.cloud.nlab.io"
+  type = "CNAME"
+  ttl = "60"
+  records = [ "${aws_elb.api.dns_name}" ]
+}
+
 resource "aws_security_group" "elb-influxdb" {
   name = "InfluxDB ELB (${var.environment})"
   description = "InfluxDB ELB (${var.environment})."
