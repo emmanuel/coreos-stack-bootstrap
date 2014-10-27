c0e4590e54e425412af41831493676f26cc5e6e6
Paul Payne <paul@payne.io>
2014-10-27 13:46:03 -0700

    Created a Kafka unlaunch_units script.

---
+++ kafka/unlaunch_units.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
+cd $SCRIPT_PATH
+
+# service templates
+fleetctl destroy kafka@.service
+fleetctl destroy kafka.presence@.service
+# service instances
+fleetctl destroy kafka@1
+fleetctl destroy kafka@2
+fleetctl destroy kafka@3
+fleetctl destroy kafka.presence@1
+fleetctl destroy kafka.presence@2
+fleetctl destroy kafka.presence@3
