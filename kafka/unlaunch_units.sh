fc59676cb88d4fccc64a7f73a1ae56532b730e92
Emmanuel Gomez <emmanuel@gomez.io>
2014-10-30 13:11:43 -0700

    feat(kafka): add unlaunch_units.sh to ease testing.

--- kafka/unlaunch_units.sh
+++ kafka/unlaunch_units.sh
@@ -3,9 +3,6 @@
 SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
 cd $SCRIPT_PATH
 
-# service templates
-fleetctl destroy kafka@.service
-fleetctl destroy kafka.presence@.service
 # service instances
 fleetctl destroy kafka@1
 fleetctl destroy kafka@2
@@ -13,3 +10,6 @@ fleetctl destroy kafka@3
 fleetctl destroy kafka.presence@1
 fleetctl destroy kafka.presence@2
 fleetctl destroy kafka.presence@3
+# service templates
+fleetctl destroy kafka@.service
+fleetctl destroy kafka.presence@.service
