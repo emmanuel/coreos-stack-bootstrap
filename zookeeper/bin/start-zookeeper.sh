717d2a9e71620abbd8c5eb0e9fdaa0eb41d78b65
Emmanuel Gomez <emmanuel@gomez.io>
2014-10-30 13:09:53 -0700

    feat(zookeeper): make start script name more specific.

---
+++ zookeeper/bin/start-zookeeper.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+printf "${ZK_SERVER_NUMBER}" > /var/lib/zookeeper/myid
+printf "Starting Zookeeper server $(cat /var/lib/zookeeper/myid); release ${ZK_RELEASE}\n"
+/opt/local/zookeeper-${ZK_RELEASE}/bin/zkServer.sh start-foreground
--- zookeeper/bin/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-printf "${ZK_SERVER_NUMBER}" > /var/lib/zookeeper/myid
-printf "Starting Zookeeper server $(cat /var/lib/zookeeper/myid)\n"
-/usr/share/zookeeper/bin/zkServer.sh start-foreground
