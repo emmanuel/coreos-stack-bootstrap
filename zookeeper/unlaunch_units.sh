58083b406d79eb2172dc25bfc9546b8ec0cfdd3f
Emmanuel Gomez <emmanuel@gomez.io>
2014-11-14 16:59:09 -0800

    Some updates to Zookeeper. Possibly broken.

--- zookeeper/unlaunch_units.sh
+++ zookeeper/unlaunch_units.sh
@@ -4,9 +4,9 @@ SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
 cd $SCRIPT_PATH
 
 # zookeeper service instances
-fleetctl destroy zookeeper.data@1
-fleetctl destroy zookeeper.data@2
-fleetctl destroy zookeeper.data@3
+# fleetctl destroy zookeeper.data@1
+# fleetctl destroy zookeeper.data@2
+# fleetctl destroy zookeeper.data@3
 fleetctl destroy zookeeper@1
 fleetctl destroy zookeeper@2
 fleetctl destroy zookeeper@3
@@ -14,6 +14,6 @@ fleetctl destroy zookeeper.presence@1
 fleetctl destroy zookeeper.presence@2
 fleetctl destroy zookeeper.presence@3
 
-fleetctl destroy zookeeper.data@.service
+# fleetctl destroy zookeeper.data@.service
 fleetctl destroy zookeeper@.service
 fleetctl destroy zookeeper.presence@.service
