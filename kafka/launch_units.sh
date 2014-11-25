5b6db1e9c1d4dd4ec66470f230bbe38ba218a2ca
Emmanuel Gomez <emmanuel@gomez.io>
2014-11-24 16:28:08 -0800

    Bind create_topics to kafka@1 so it can start afterward.

--- kafka/launch_units.sh
+++ kafka/launch_units.sh
@@ -5,12 +5,13 @@ cd $SCRIPT_PATH
 
 # service templates
 fleetctl submit kafka@.service
+fleetctl submit kafka.create_topics@.service
 fleetctl submit kafka.presence@.service
 # service instances
 fleetctl start kafka@1
 fleetctl start kafka@2
 fleetctl start kafka@3
+fleetctl start kafka.create_topics@1.service
 fleetctl start kafka.presence@1
 fleetctl start kafka.presence@2
 fleetctl start kafka.presence@3
-fleetctl start kafka.create_topics.service
