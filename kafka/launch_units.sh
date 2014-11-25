1c8c9a21c8f8ec0be28b30d0373919884ad0a183
Emmanuel Gomez <emmanuel@gomez.io>
2014-11-24 16:43:11 -0800

    Introduce a data volume for Kafka.

--- kafka/launch_units.sh
+++ kafka/launch_units.sh
@@ -4,10 +4,14 @@ SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
 cd $SCRIPT_PATH
 
 # service templates
+fleetctl submit kafka.data@.service
 fleetctl submit kafka@.service
 fleetctl submit kafka.create_topics@.service
 fleetctl submit kafka.presence@.service
 # service instances
+fleetctl start kafka.data@1
+fleetctl start kafka.data@2
+fleetctl start kafka.data@3
 fleetctl start kafka@1
 fleetctl start kafka@2
 fleetctl start kafka@3
