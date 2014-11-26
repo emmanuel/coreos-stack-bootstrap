9ca1006b7c5d1f471c01793d4a132831f4b5eedd
Emmanuel Gomez <emmanuel@gomez.io>
2014-11-25 16:25:08 -0800

    Schedule zookeeper.presence whereever zookeeper.data is.

--- zookeeper/launch_units.sh
+++ zookeeper/launch_units.sh
@@ -4,13 +4,13 @@ SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
 cd $SCRIPT_PATH
 
 # zookeeper templates
-# fleetctl submit zookeeper.data@.service
+fleetctl submit zookeeper.data@.service
 fleetctl submit zookeeper@.service
 fleetctl submit zookeeper.presence@.service
 # zookeeper service instances
-# fleetctl start zookeeper.data@1
-# fleetctl start zookeeper.data@2
-# fleetctl start zookeeper.data@3
+fleetctl start zookeeper.data@1
+fleetctl start zookeeper.data@2
+fleetctl start zookeeper.data@3
 fleetctl start zookeeper.presence@1
 fleetctl start zookeeper.presence@2
 fleetctl start zookeeper.presence@3
