c0b4d2ed234951071eb4abc007cd484ee8ea223d
Emmanuel Gomez <emmanuel@gomez.io>
2014-11-25 16:41:44 -0800

    Rename influxdb.db_create -> influxdb.create_db.
    What can I say? I like it better. Also, Kafka is
    already set up that way (kafka.create_topics).

--- influxdb/unlaunch_units.sh
+++ influxdb/unlaunch_units.sh
@@ -5,13 +5,13 @@ cd $SCRIPT_PATH
 
 # influxdb service instances
 # fleetctl destroy influxdb.data@1
-fleetctl destroy influxdb.db_create@1
+fleetctl destroy influxdb.create_db@1
 fleetctl destroy influxdb.presence@1
 fleetctl destroy influxdb.elb@1
 fleetctl destroy influxdb@1
 
 # fleetctl destroy influxdb.data@.service
-fleetctl destroy influxdb.db_create@.service
+fleetctl destroy influxdb.create_db@.service
 fleetctl destroy influxdb.presence@.service
 fleetctl destroy influxdb.elb@.service
 fleetctl destroy influxdb@.service
