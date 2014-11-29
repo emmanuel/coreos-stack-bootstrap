af3df0400e16c813d322bcad031a3df2e803b451
Emmanuel Gomez <emmanuel@gomez.io>
2014-11-28 16:13:15 -0800

    Rename to standardize on `start-X.sh`, e.g., `kafka`.

--- kafka/bin/start-kafka.sh
+++ kafka/bin/start-kafka.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-KAFKA_BROKER_ID=$RANDOM
+KAFKA_STORED_BROKER_ID=$(cat /kafka/conf/broker_id)
+KAFKA_BROKER_ID=${KAFKA_STORED_BROKER_ID:$RANDOM}
+echo "$KAFKA_BROKER_ID" > /kafka/conf/broker_id
 echo "Starting Kafka broker with ID $KAFKA_BROKER_ID"
 echo "broker.id=$KAFKA_BROKER_ID" >> $KAFKA_HOME/config/server.properties
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
