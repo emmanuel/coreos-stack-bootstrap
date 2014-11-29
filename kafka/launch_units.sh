7ba36a85869ba29643273af11a1ee01967cbb29a
Emmanuel Gomez <emmanuel@gomez.io>
2014-11-29 01:26:09 -0800

    Break out /kafka/conf volume.

--- kafka/launch_units.sh
+++ kafka/launch_units.sh
@@ -5,6 +5,7 @@ cd $SCRIPT_PATH
 
 # service templates
 fleetctl submit kafka.data@.service
+fleetctl submit kafka.conf@.service
 fleetctl submit kafka@.service
 fleetctl submit kafka.create_topics@.service
 fleetctl submit kafka.presence@.service
@@ -12,10 +13,13 @@ fleetctl submit kafka.presence@.service
 fleetctl start kafka.data@1
 fleetctl start kafka.data@2
 fleetctl start kafka.data@3
+fleetctl start kafka.conf@1
+fleetctl start kafka.conf@2
+fleetctl start kafka.conf@3
 fleetctl start kafka@1
 fleetctl start kafka@2
 fleetctl start kafka@3
-fleetctl start kafka.create_topics@1.service
 fleetctl start kafka.presence@1
 fleetctl start kafka.presence@2
 fleetctl start kafka.presence@3
+fleetctl start kafka.create_topics@1.service
