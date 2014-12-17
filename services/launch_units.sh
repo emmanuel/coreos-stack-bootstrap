00070e48c04b731f818ae06689d3b273eb3b553b
Paul Payne <paul@payne.io>
2014-12-16 16:33:32 -0800

    Service cluster launch and unlaunch unit scripts.

---
+++ services/launch_units.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$FLEETCTL_TUNNEL" ]; then
+    echo
+    echo "You must set FLEETCTL_TUNNEL (a resolvable address to one of your CoreOS instances)"
+    echo "e.g.:"
+    echo "export FLEETCTL_TUNNEL=1.2.3.4"
+    echo
+    exit 1
+fi
+
+SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
+cd $SCRIPT_PATH/.
+
+fleetctl start units/vulcan/vulcand.service
+fleetctl start units/vulcan/vulcand.elb.service
