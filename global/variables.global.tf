66865df543fa979e145faa71e97916100e752071
Emmanuel Gomez <emmanuel@gomez.io>
2014-12-17 11:51:20 -0800

    Update AMIs to CoreOS v522.1.0.

--- global/variables.global.tf
+++ global/variables.global.tf
@@ -20,15 +20,15 @@ variable "allow_ssh_from" {
 # These are alpha channel, HVM virtualization
 variable "amis" {
   default = {
-    eu-central-1 = "ami-10c0f10d"
-    ap-northeast-1 = "ami-da6566db"
-    sa-east-1 = "ami-99da6a84"
-    ap-southeast-2 = "ami-a9c8a193"
-    ap-southeast-1 = "ami-957559c7"
-    us-east-1 = "ami-f669f29e"
-    us-west-2 = "ami-d9f8afe9"
-    us-west-1 = "ami-ad0516e8"
-    eu-west-1 = "ami-f4853883"
+    eu-central-1 = "ami-1a97a607"
+    ap-northeast-1 =  "ami-96e2ef97"
+    sa-east-1 = "ami-5d56e640"
+    ap-southeast-2 =  "ami-dd4a22e7"
+    ap-southeast-1 = "ami-9fd6facd"
+    us-east-1 =  "ami-1299fe7a"
+    us-west-2 = "ami-c71d4cf7"
+    us-west-1 =  "ami-37766472"
+    eu-west-1 = "ami-748b3403"
   }
 }
 variable "aws_ec2_keypair" {
@@ -36,6 +36,9 @@ variable "aws_ec2_keypair" {
 }
 variable "aws_route53_zone_id_cloud_nlab_io" {
   default = "Z23E6ZIBKPSZQE"
+}
+variable "vpc_zone_identifier" {
+  default = "vpc-9111f3f4"
 }
 variable "aws_sns_topic_contol_autoscaling_events_arn" {
   default = "arn:aws:sns:us-west-2:731703850786:control-autoscaling-events"
