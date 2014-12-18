9923a4b3d6a819866219b71139e16b09c82d7cd3
Emmanuel Gomez <emmanuel@gomez.io>
2014-12-18 15:44:33 -0800

    Fix some typos in control/launch_units.sh.

--- control/launch_units.sh
+++ control/launch_units.sh
@@ -18,7 +18,7 @@ fleetctl start units/skydns.service
 
 fleetctl submit units/influxdb{.elb,.presence,.create_db,}@.service
 fleetctl start influxdb{.elb,.presence,}@1
-fleetctl start units/kafka.create_topics@1.service
+fleetctl start units/influxdb.create_db@1.service
 
 fleetctl start units/cadvisor.service
 fleetctl start units/sysinfo_influxdb.service
@@ -33,7 +33,7 @@ fleetctl start units/kafka.create_topics@1
 fleetctl start units/syslog_gollector.service
 
 fleetctl submit units/elasticsearch{.data,.presence,}@.service
-fleetctl start units/elasticsearch{.data,.presence,}@{1..3}.service
+fleetctl start units/elasticsearch{.data,.presence,}@{1..2}
 
 fleetctl submit units/logstash{.presence,}@.service
-fleetctl start units/logstash{.presence,}@{1..3}.service
+fleetctl start units/logstash{.presence,}@{1..2}
