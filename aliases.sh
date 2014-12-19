5c858fef3a527f63adccd4d33409df5d3dcc1243
Paul Payne <paul@payne.io>
2014-12-19 11:19:29 -0800

    Fix tfdestroyplan and add tfrefresh.

--- aliases.sh
+++ aliases.sh
@@ -2,4 +2,5 @@
 
 alias tfplan='terraform plan --var-file ../cluster_values.tfvars --var-file=../terraform.global.tfvars --var-file=terraform.local.tfvars'
 alias tfapply='terraform apply --var-file ../cluster_values.tfvars --var-file=../terraform.global.tfvars --var-file=terraform.local.tfvars'
-alias tfdestroyplan='terraform plan --var-file ../cluster_values.tfvars --var-file=../terraform.global.tfvars --var-file=terraform.global.tfvars -destroy --out=destroy.tfplan'
+alias tfrefresh='terraform refresh --var-file ../cluster_values.tfvars --var-file=../terraform.global.tfvars --var-file=terraform.local.tfvars'
+alias tfdestroyplan='terraform plan --var-file ../cluster_values.tfvars --var-file=../terraform.global.tfvars --var-file=terraform.local.tfvars -destroy --out=destroy.tfplan'
