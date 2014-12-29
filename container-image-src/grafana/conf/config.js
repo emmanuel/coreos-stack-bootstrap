e9179d0f5c5187ea810b3a268a722657a22bf019
Emmanuel Gomez <emmanuel@gomez.io>
2014-12-28 23:13:14 -0800

    Configure Grafana to connect to InfluxDB via SSH tunnel.

--- container-image-src/grafana/conf/config.js
+++ container-image-src/grafana/conf/config.js
@@ -11,19 +11,19 @@ function (Settings) {
     datasources: {
       sysinfo: {
         type: 'influxdb',
-        url: "http://influxdb.baavm.cloud.nlab.io:8086/db/sysinfo",
+        url: "http://localhost:8086/db/sysinfo",
         username: 'root',
         password: 'root'
       },
       cadvisor: {
         type: 'influxdb',
-        url: "http://influxdb.baavm.cloud.nlab.io:8086/db/cadvisor",
+        url: "http://localhost:8086/db/cadvisor",
         username: 'root',
         password: 'root'
       },
       grafana: {
         type: 'influxdb',
-        url: "http://influxdb.baavm.cloud.nlab.io:8086/db/grafana",
+        url: "http://localhost:8086/db/grafana",
         username: 'root',
         password: 'root',
         grafanaDB: true
