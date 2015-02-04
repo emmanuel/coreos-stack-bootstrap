d2c8bdb3f8527b92219aeedb7eb1e01ab0ed9cde
Emmanuel Gomez <emmanuel@gomez.io>
2015-02-04 15:22:44 -0800

    Align aliases.sh.

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias tfplan='terraform plan --var-file ../cluster_id.tfvars --var-file=../keys.tfvars'
-alias tfapply='terraform apply --var-file ../cluster_id.tfvars --var-file=../keys.tfvars'
-alias tfrefresh='terraform refresh --var-file ../cluster_id.tfvars --var-file=../keys.tfvars'
-alias tfdestroyplan='terraform plan --var-file ../cluster_id.tfvars --var-file=../keys.tfvars -destroy --out=destroy.tfplan'
+alias        tfplan='terraform plan --var-file ../cluster_id.tfvars --var-file=../keys.tfvars --var-file=../visible.tfvars'
+alias      tfapply='terraform apply --var-file ../cluster_id.tfvars --var-file=../keys.tfvars --var-file=../visible.tfvars'
+alias  tfrefresh='terraform refresh --var-file ../cluster_id.tfvars --var-file=../keys.tfvars --var-file=../visible.tfvars'
+alias tfdestroyplan='terraform plan --var-file ../cluster_id.tfvars --var-file=../keys.tfvars --var-file=../visible.tfvars -destroy --out=destroy.tfplan'
