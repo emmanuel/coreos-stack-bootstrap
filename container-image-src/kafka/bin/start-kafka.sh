1cbd2902f14bd46884392a5146921e551b0c44b4
Emmanuel Gomez <emmanuel@gomez.io>
2015-03-06 16:36:59 -0800

    Ditch attempt to reuse Kafka broker id on service restart.

--- container-image-src/kafka/bin/start-kafka.sh
+++ container-image-src/kafka/bin/start-kafka.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 KAFKA_STORED_BROKER_ID=$(cat /kafka/conf/broker_id)
-KAFKA_BROKER_ID=${KAFKA_STORED_BROKER_ID:$RANDOM}
+KAFKA_BROKER_ID=${RANDOM}
 echo "$KAFKA_BROKER_ID" > /kafka/conf/broker_id
 echo "Starting Kafka broker with ID $KAFKA_BROKER_ID"
-echo "broker.id=$KAFKA_BROKER_ID" >> $KAFKA_HOME/config/server.properties
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+echo "broker.id=$KAFKA_BROKER_ID" >> /etc/kafka/server.properties
+/usr/bin/kafka-server-start /etc/kafka/server.properties
