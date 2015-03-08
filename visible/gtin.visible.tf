c7cade836286c7c896c7d07a5b22c69c2f46f2be
Emmanuel Gomez <emmanuel@gomez.io>
2015-03-08 15:09:31 -0700

    fixup! Add support for gtin

--- visible/gtin.visible.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "vulcand_visible_gtin" {
-  zone_id = "${var.aws_route53_zone_id_cloud_nlab_io}"
-  name = "gtin.api.cloud.nlab.io"
-  type = "CNAME"
-  ttl = "900"
-  records = [ "${aws_elb.vulcand_visible.dns_name}" ]
-}
