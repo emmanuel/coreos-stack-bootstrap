0d5cfb9418b3d97f2857ab7fc72a56692871e8a4
Emmanuel Gomez <emmanuel@gomez.io>
2015-07-04 15:39:33 -0700

    Remove superfluous prefix on subnet-egress-nat’s cloud_config.

--- subnet-egress-nat/main.tf
+++ subnet-egress-nat/main.tf
@@ -16,17 +16,14 @@ module "subnet-egress-nat-asg" {
     source = "../tf_aws_subnet_asg"
 
     launch_config_name = "${var.stack_name}-subnet-egress-nat-launch_config"
-    # launch_config_name = "subnet-egress-nat-launch_config"
     ami_id = "${var.coreos_ami_id}"
     instance_type = "${var.ec2_instance_type}"
     iam_instance_profile = ""
     key_name = "${var.ec2_key_name}"
     security_group_ids = "${terraform_remote_state.vpc.output.subnet_egress_nat_instances_sg_id}"
     user_data = "${template_file.subnet-egress-nat-test-cloud_config.rendered}"
-    # user_data = ""
 
     asg_name = "${var.stack_name}-subnet-egress-nat-asg"
-    # asg_name = "subnet-egress-nat-asg"
     asg_max_size = 2
     asg_min_size = 1
     asg_desired_capacity = 1
@@ -39,7 +36,7 @@ module "subnet-egress-nat-asg" {
 }
 
 resource "template_file" "subnet-egress-nat-test-cloud_config" {
-    filename = "subnet-egress-nat-test-cloud_config.yaml.tmpl"
+    filename = "cloud_config.yaml.tmpl"
 
     vars {
         network_prefix = "172.33"
