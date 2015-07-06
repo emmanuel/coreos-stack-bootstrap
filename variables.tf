9d817f52f7b069aede74b29be7f19a2dd23a2378
Emmanuel Gomez <emmanuel@gomez.io>
2015-07-06 10:40:36 -0700

    Reorder some variables.
    I was trying to sort by descending generality, but
    it is hard to compare their generality. E.g., is
    `stack_name` more or less general than `availability_zones`?

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "aws_region" {}
 
-variable "ec2_key_name" {}
-variable "ec2_instance_type" {}
 variable "stack_name" {}
+variable "availability_zones" {}
+variable "ec2_instance_type" {}
 variable "coreos_ami_id" {}
+variable "ec2_key_name" {}
+variable "etcd_discovery_url" {}
 
 variable "autoscaling_group_health_check_type" {
     default = "EC2"
@@ -14,6 +16,3 @@ variable "health_check_grace_period" {
 variable "default_cooldown" {
     default = 300
 }
-
-variable "availability_zones" {}
-variable "etcd_discovery_url" {}
